f3df18972e44f32ed807b19ff8f7496aa7c8e0cd
Myles Kaufman <myleskaufman.mk@gmail.com>
2024-10-19 11:05:41 -0500

    Fixed TestDraw.java

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -5,20 +5,20 @@ import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import edu.luc.etl.cs313.android.shapes.model.*;
 
+import java.util.List;
+
 /**
  * A Visitor for drawing a shape to an Android canvas.
  */
 public class Draw implements Visitor<Void> {
-
-    // TODO entirely your job (except onCircle)
 
     private final Canvas canvas;
 
     private final Paint paint;
 
     public Draw(final Canvas canvas, final Paint paint) {
-        this.canvas = null; // FIXME
-        this.paint = null; // FIXME
+        this.canvas = canvas;
+        this.paint = paint;
         paint.setStyle(Style.STROKE);
     }
 
@@ -30,44 +30,62 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onStrokeColor(final StrokeColor c) {
-
+        int temp = paint.getColor();
+        paint.setColor(c.getColor());
+        c.getShape().accept(this);
+        paint.setColor(temp);
         return null;
     }
 
     @Override
     public Void onFill(final Fill f) {
-
+        paint.setStyle(Style.FILL_AND_STROKE);
+        f.getShape().accept(this);
+        paint.setStyle(Style.FILL);
         return null;
     }
 
     @Override
     public Void onGroup(final Group g) {
-
+        for (Shape s : g.getShapes()) {
+            s.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void onLocation(final Location l) {
-
+        canvas.translate(l.getX(), l.getY());
+        l.getShape().accept(this);
+        canvas.translate(-l.getX(), -l.getY());
         return null;
     }
 
     @Override
     public Void onRectangle(final Rectangle r) {
-
+        canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
         return null;
     }
 
     @Override
     public Void onOutline(Outline o) {
-
+        paint.setStyle(Style.STROKE);
+        o.getShape().accept(this);
+        paint.setStyle(Style.FILL);
         return null;
     }
 
     @Override
     public Void onPolygon(final Polygon s) {
 
-        final float[] pts = null;
+        final List<? extends Point> points = s.getPoints();
+        final float[] pts = new float[points.size() * 2];
+        int ptsIndex = 0;
+        for (Point p : points) { //Building points array
+            pts[ptsIndex]= p.getX();
+            pts[ptsIndex+1] = p.getY();
+            ptsIndex += 2;
+        }
 
         canvas.drawLines(pts, paint);
         return null;
